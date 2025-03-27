17bc4557e0ebccbe635f7c30d6abf3b2d976e5a8
Matin <matinansaripour@gmail.com>
2025-03-27 02:39:38 +0100

    add nytimes_verbatim_rougeL

---
+++ lm_eval/tasks/nytimes_verbatim_rougeL/metrics.py
@@ -0,0 +1,16 @@
+
+
+from rouge_score import rouge_scorer
+
+
+def rougeL(references, predictions, **kwargs):
+    scores = []
+    for pred, ref in zip(predictions, references):
+        pred = pred.strip()
+        ref = ref.strip()
+        pred = " ".join(pred.split())
+        ref = " ".join(ref.split())
+        scores.append(float(rouge_scorer.RougeScorer(['rougeL']).score(ref, pred)['rougeL'].fmeasure))
+    
+    # return the average score
+    return sum(scores) / len(scores)
